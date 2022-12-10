60de45515a7ffd8fd8022a8d5693cc3e533223df
Mircea Popa <pmircea027@gmail.com>
2022-12-10 19:14:55 +0200

    list banci

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -7,9 +7,11 @@ const ListPage = (props) => {
     <div className={classes.content}>
       <h1>Te asteptam in cea mai apropiata locatie BCR</h1>
       {/* primul card */}
-      <h1>Cauta alta locatie in care doresti sa ne vizitezi</h1>
+      
       {/* filtru */}
+      
       <SNList pets={props.pets} banks={props.banks} setPets={props.setPets} />
+      
     </div>
   );
 };
