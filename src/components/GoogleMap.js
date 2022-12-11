dea65e567d4182039851df78cb77571b92780dff
Mircea Popa <pmircea027@gmail.com>
2022-12-11 06:53:52 +0200

    update

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -7,7 +7,10 @@ const containerStyle = {
   height: "400px",
 };
 
-
+const defaultMapOptions = {
+  fullscreenControl: false,
+};
+const demoFancyMapStyles = require("../components/demoFancyMapStyles.json");
 const MyMap = (props) => {
   console.log(props)
   const center = {
@@ -40,14 +43,21 @@ const MyMap = (props) => {
   return isLoaded ? (
     <GoogleMap
       mapContainerStyle={containerStyle}
+
       center={center}
-      zoom={13}
+      zoom={11}
+      fullscreenControl={false}
+      defaultMapOptions={defaultMapOptions}
       onLoad={onLoad}
       onUnmount={onUnmount}
     >
       {/* Child components, such as markers, info windows, etc. */}
-      <Marker position={center}></Marker>
+      <Marker
+      position={center}
+      ></Marker>
       {/* <Marker position={center2}></Marker> */}
+
+      
     </GoogleMap>
   ) : (
     <></>
